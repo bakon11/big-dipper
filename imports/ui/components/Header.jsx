8d54fcf6be55830af726cef00c988007d1a6173a
baKon <maciej@geoskygps.com>
2021-07-22 20:59:04 -0500

    Update Header.jsx

--- imports/ui/components/Header.jsx
+++ imports/ui/components/Header.jsx
@@ -180,23 +180,23 @@ export default class Header extends Component {
                             <NavLink style={{ color: "#000000" }} to="/validators"><T>navbar.validators</T></NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{ color: "#000000" }} tag="Link" to="/blocks"><T>navbar.blocks</T></NavLink>
+                            <NavLink style={{ color: "#000000" }} tag={Link} to="/blocks"><T>navbar.blocks</T></NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{ color: "#000000" }} tag="Link" to="/transactions"><T>navbar.transactions</T></NavLink>
+                            <NavLink style={{ color: "#000000" }} tag={Link} to="/transactions"><T>navbar.transactions</T></NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{ color: "#000000" }} tag="Link" to="/proposals"><T>navbar.proposals</T></NavLink>
+                            <NavLink style={{ color: "#000000" }} tag={Link} to="/proposals"><T>navbar.proposals</T></NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{ color: "#000000" }} tag="Link" to="/voting-power-distribution"><T>navbar.votingPower</T></NavLink>
+                            <NavLink style={{ color: "#000000" }} tag={Link} to="/voting-power-distribution"><T>navbar.votingPower</T></NavLink>
                         </NavItem>
                         <NavItem id="user-acconut-icon">
                             {!signedInAddress?<Button className="sign-in-btn" color="link" size="lg" onClick={() => {this.setState({isSignInOpen: true})}}><i className="material-icons">vpn_key</i></Button>:
                                 <span>
                                     <span className="d-lg-none">
                                         <i className="material-icons large d-inline">account_circle</i>
-                                        <Link to={`/account/${signedInAddress}`}> {signedInAddress}</Link>
+                                        <Link style={{ color: "#000000" }} to={`/account/${signedInAddress}`}> {signedInAddress}</Link>
                                         <Button className="float-right" color="link" size="sm" onClick={this.signOut}><i className="material-icons">exit_to_app</i></Button>
                                     </span>
                                     <span className="d-none d-lg-block">
